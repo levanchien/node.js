429bf483e3627820448aef4774c43184a3136637
Chien <levanchien95.cit@gmail.com>
2021-02-24 20:05:16 +0700

    Update

--- src/async-await/promise.js
+++ src/async-await/promise.js
@@ -1,20 +1,14 @@
-const promise1 = new Promise(resolve => setTimeout(() => {
-    console.log('promise1');
-    resolve(1);
-}, 1000));
-const promise2 = new Promise(resolve => setTimeout(() => {
-    console.log('promise2');
-    resolve(2);
-}, 2000));
-const promise3 = new Promise(resolve => setTimeout(() => {
-    console.log('promise3');
-    resolve(3);
-}, 3000));
+const promise = ms => new Promise(resolve => setTimeout(() => {
+    resolve(ms);
+}, ms));
 
 // Chain
-promise1
-    .then(res => promise2)
-    .then(promise3)
-    .catch(error => console.log(console.error(error)));
-Promise.all([promise3, promise1, promise2]).then(res => console.log(res));
-Promise.race([promise3, promise1, promise2]).then(res => console.log(res));
+promise(1000)
+    .then(res => promise(res * 2))
+    .then(res => console.log(res))
+    .then(res => console.log(res))
+    .catch(error => console.log(error));
+// Xu ly song song
+Promise.all([promise(10), promise(20), promise(30)]).then(res => console.log(res));
+// Thang nao nhanh thi lay
+Promise.race([promise(10), promise(20), promise(30)]).then(res => console.log(res));
