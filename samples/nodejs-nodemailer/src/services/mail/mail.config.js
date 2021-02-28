1cd36c9a4b00ad9c78fcf3513e0da170f57a00db
Chien <levanchien95.cit@gmail.com>
2021-02-28 16:24:15 +0700

    Add nodejs nodemailer gmail demo

--- samples/nodejs-nodemailer/src/services/mail/mail.config.js
+++ samples/nodejs-nodemailer/src/services/mail/mail.config.js
@@ -7,11 +7,11 @@ module.exports.transporter = (service) => {
         service: 'gmail',
         auth: {
           type: "OAuth2",
-          user: process.env.MAIL_EMAIL,
-          clientId: " levanchien",
-          clientSecret: "levanchien",
-          refreshToken: "1//04IsxjtJPBH9dCgYIARAAGAQSNwF-L9Ir_KPQ-ErdosadsaasdadasddsadadadadtgJA8SLSb0k_6vM_aiFgXQkI1zXfXl3fRCQaDrdOFPqD14jGwchh01_GqU",
-          accessToken: "ya29.A0AfH6SMAUHSAgfDHjiW-cNru8fvnmcrZCfZZLaUcvfw54kxmOvO_xBmw3LFQKLmZJDVW_4i6-MVPugBEx5reTK918kSthejOXwoTdx6GejaAP-DHPWRq3HAp04e8CwfnBqfPZvx6HaOVOx4S0PWjfX1EbPmA5" //access token variable we defined earlier
+          user: process.env.GMAIL_EMAIL,
+          clientId: process.env.GMAIL_CLIENT_ID,
+          clientSecret: process.env.GMAIL_CLIENT_SECRET,
+          refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+          accessToken: process.env.GMAIL_ACCESS_TOKEN
         },
       });
   }
